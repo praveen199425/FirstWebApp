7bb0af9205fbbac941b702f1e73f6f55762bd3ea
praveen cm <praveen.cm@datalyzer.com>
2022-04-19 23:25:17 +0530

    Added Producttype,Productbrand,ProductRepository,Migrated it and seeddata to databse

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using Infrastructure.Data;
 using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Core.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,22 +14,36 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class ProductsController :ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductsController(StoreContext context)
+        public IProductRepository _Repo { get; }
+
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            _Repo = repo;
+            
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products=await _context.Productsb.ToListAsync();
+            var products=await _Repo.GetProductsAsync();
             return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Productsb.FindAsync(id);
+            return await _Repo.GetProductByIdAsync(id);
+        }
+         [HttpGet("brands")]
+        public async Task<ActionResult<List<Product>>> GetProductBrands()
+        {
+            var products=await _Repo.GetProductBrandsAsync();
+            return Ok(products);
+        }
+        [HttpGet("types")]
+        public async Task<ActionResult<List<Product>>> GetProductTypes()
+        {
+            var products=await _Repo.GetProductTypesAsync();
+            return Ok(products);
         }
     }
 }
